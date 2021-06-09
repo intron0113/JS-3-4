40a6aad69796b78491b66b235c93ecc04de61aa6
intron0113 <83868900+intron0113@users.noreply.github.com>
2021-06-09 13:36:29 +0900

    1st

---
+++ main.js
@@ -0,0 +1,93 @@
+`use strict`;
+{
+  const inputBox = document.getElementById('input-todo');
+  const addButton = document.getElementById('add-button');
+  const addTaskTarget = document.getElementById('addTask-target');
+  const todos = [];
+
+  addButton.addEventListener('click', () => {
+    const todo = { id: todos.length, comment: inputBox.value, status: '作業中' };
+    inputBox.focus();
+    if (inputBox.value === '') {
+      console.log('タスクを入力してください！');
+      return;
+    }
+    todos.push(todo);
+    inputBox.value = '';
+    radioChange();
+  });
+
+  // 「todos」の中身を一覧表示するための関数を用意する //
+  const showTodos = (selecttodos) => {
+    addTaskTarget.textContent = '';
+    selecttodos.forEach((todo) => {
+      const tableRecord = document.createElement('tr');
+      addTaskTarget.appendChild(tableRecord);
+      const tableId = document.createElement('td');
+      const tableComment = document.createElement('td');
+      const tableStatus = document.createElement('td');
+      const tableAction = document.createElement('td');
+
+      tableId.textContent = todo.id;
+      tableComment.textContent = todo.comment;
+      tableRecord.appendChild(tableId);
+      tableRecord.appendChild(tableComment);
+      tableRecord.appendChild(tableStatus);
+      tableRecord.appendChild(tableAction);
+
+      tableStatus.appendChild(createStatusButton(todo));
+      tableAction.appendChild(createDeleteButton(todo.id));
+    });
+  };
+
+  //「状態機能」を管理するボタンを生成する関数
+  const createStatusButton = (todo) => {
+    const statusButton = document.createElement('button');
+    statusButton.innerText = todo.status;
+    statusButton.addEventListener('click', () => {
+      if (todo.status === '作業中') {
+        todo.status = '完了';
+      } else {
+        todo.status = '作業中';
+      }
+      showTodos(todos);
+      console.log(todos);
+    });
+    return statusButton;
+  };
+
+  //「削除機能」を管理するボタンを生成する関数
+  const createDeleteButton = (index) => {
+    const createdeletebutton = document.createElement("button");
+    createdeletebutton.textContent = "削除";
+    createdeletebutton.addEventListener("click", () => {
+      todos.splice(index, 1);
+      showTodos(todos);
+      todos.reduce((Idnum, todo) => (todo.id = Idnum + 1), -1);
+      showTodos(todos);
+    });
+    return createdeletebutton;
+  };
+
+  //ラジオボタンを押した際にフィルタリングする為の関数
+  function radioChange() {
+    const radio1_1 = document.getElementById("radio-all-select");
+    const radio1_2 = document.getElementById("adio-working-select");
+    const radio1_3 = document.getElementById("radio-done-select");
+
+    if (radio1_1.checked) {
+      todos.slice();
+      return showTodos(todos);
+    } else if (radio1_2.checked) {
+      let filterdoing = todos.filter((todo) => {
+        return todo.status === '作業中';
+      });
+      return showTodos(filterdoing);
+    } else if (radio1_3.checked) {
+      let filterdone = todos.filter((todo) => {
+        return todo.status === '完了';
+      });
+      return showTodos(filterdone);
+    }
+  }
+}
